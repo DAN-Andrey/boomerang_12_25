30cee577616762044184d7534a4bd55db5bc161a
oralova51 <oralova51@gmail.com>
2025-12-05 11:15:53 +0200

    function findOrCreate is realized

--- run.js
+++ run.js
@@ -1,9 +1,29 @@
-// Основной файл.
-// Запускает игру.
+// run.js
 const Game = require('./src/Game');
+const readline = require('readline');
+const { Hero, GameResult } = require('./db/models');
 
-// Инициализация игры с настройками.
-const game = new Game();
+// Функция для ввода имени через консоль
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// Запуск игры.
-game.play();
+rl.question('Введите имя игрока: ', (name) => {
+  // Создаём объект игрока
+  const player = { name, score: 0, level: 1 };
+
+  // Инициализируем игру с этим игроком
+  Hero.findOrCreate({ where: { name: player.name }, defaults: player })
+    .then(([user, created]) => {
+      console.log('user', user.get());
+      console.log('created', created);
+    })
+    .catch(console.error);
+
+  // Сразу запускаем игру
+  const game = new Game(player);
+  game.play();
+
+  rl.close();
+});
