bc1a9c05d45a966e4f934243d4c0fce76c3709d7
oralova51 <oralova51@gmail.com>
2025-12-05 10:39:00 +0200

    db is ready for REAL USER

--- db/seeders/20251205075127-Hero.js
+++ db/seeders/20251205075127-Hero.js
@@ -3,21 +3,23 @@
 const { faker } = require('@faker-js/faker');
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-     const arrOfHeros = [];
+const arrOfHeros = [];
 
-  for (let index = 0; index < 10; index++) {
-      arrOfHeros.push({
-        name: faker.internet.username(),
-        bestScore: Math.floor(Math.random() * 10) + 1,
-        totalGames: Math.floor(Math.random() * 10) + 1
-      });
-    }
-      await queryInterface.bulkInsert('Heros', arrOfHeros, {});
-  }
-
-  async down (queryInterface) {
-  await queryInterface.bulkDelete('Heros', null, {});
-  }
+for (let index = 0; index < 10; index++) {
+  arrOfHeros.push({
+    name: faker.internet.username(),
+    bestScore: Math.floor(Math.random() * 10) + 1,
+    totalGames: Math.floor(Math.random() * 10) + 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
 }
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Heros', arrOfHeros, {});
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Heros', null, {});
+  },
+};
--- db/seeders/20251205075250-GameResult.js
+++ db/seeders/20251205075250-GameResult.js
@@ -1,19 +1,20 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+const arrOfResults = [];
 
+for (let index = 0; index < 10; index++) {
+  arrOfResults.push({
+    playerId: Math.floor(Math.random() * 10) + 1,
+    enemiesDefeated: Math.floor(Math.random() * 10) + 1,
+    durationSec: Math.floor(Math.random() * 10) + 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const arrOfResults = [];
-    
-    for (let index = 0; index < 10; index++) {
-      arrOfResults.push({
-        playerId:  Math.floor(Math.random() * 10) + 1,
-        enemiesDefeated: Math.floor(Math.random() * 10) + 1,
-        durationSec: Math.floor(Math.random() * 10) + 1,
-      });
-    }
-    await queryInterface.bulkInsert('GameResults', arrOfResults,{},);
+    await queryInterface.bulkInsert('GameResults', arrOfResults, {});
   },
 
   async down(queryInterface) {
