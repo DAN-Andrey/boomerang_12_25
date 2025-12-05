85ad5f34f20ba89a48890b99b7c77b7642a47c10
mdirect <muza33@ya.ru>
2025-12-05 15:17:57 +0300

    Рабочая версия

--- run.js
+++ run.js
@@ -1,27 +1,10 @@
-// run.js
 const Game = require('./src/Game');
-const readline = require('readline');
+const runInteractiveConsole = require('./src/keyboard');
 const { createHero } = require('./db/repo/createHero');
 const { updateResultsAfterGame } = require('./db/repo/updateResultsAfterGame');
 
-// Функция для ввода имени через консоль
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// Сразу запускаем игру
+const game = new Game();
 
-rl.question('Введите имя игрока: ', async (name) => {
-  // Создаём объект игрока
-  const player = { name, bestScore: 0, totalGames: 1 };
-
-  // Инициализируем игру с этим игроком
-  await createHero(player);
-
-  // Сразу запускаем игру
-  const game = new Game(player);
-  game.play();
-
-  rl.close();
-
-  await updateResultsAfterGame(player);
-});
+runInteractiveConsole(game.hero);
+game.play();
