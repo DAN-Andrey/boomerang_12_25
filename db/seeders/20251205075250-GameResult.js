57771c8dfbda0c93e1b858e64d56cbf70f92490f
oralova51 <oralova51@gmail.com>
2025-12-05 10:08:08 +0200

    seed gameResult

--- db/seeders/20251205075250-GameResult.js
+++ db/seeders/20251205075250-GameResult.js
@@ -1,21 +1,25 @@
 'use strict';
 
+const { faker } = require('@faker-js/faker');
+
 /** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(
-      'GameResults',
-      [
-        {
-          name: 'John Doe',
-          isBetaMember: false,
-        },
-      ],
-      {},
-    );
+    const arrOfResults = [];
+    
+    for (let index = 0; index < 10; index++) {
+      arrOfResults.push({
+        playerId:  Math.floor(Math.random() * 10) + 1,
+        enemiesDefeated: Math.floor(Math.random() * 10) + 1,
+        durationSec: Math.floor(Math.random() * 10) + 1,
+      });
+    }
+
+    await queryInterface.bulkInsert('GameResults', arrOfResults,{},);
   },
 
   async down(queryInterface) {
-    await queryInterface.bulkDelete('People', null, {});
+    await queryInterface.bulkDelete('GameResults', null, {});
   },
 };
